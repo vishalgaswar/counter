c571fe562bdc3dbc4bc21aced99a463b289f0153
Vishal G. Aswar <vishalaswar25@gmail.com>
2021-03-29 14:08:27 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,6 +20,18 @@ btns.forEach(function (btn) {
           count = 0;
       } 
 
+      if(count > 0 )
+      {
+          value.style.color = "green";
+      }
+      if(count < 0)
+      {
+          value.style.color = "red";
+      }
+      if(count === 0)
+      {
+          value.style.color = "black";
+      }
       value.textContent = count;
    });
 });
