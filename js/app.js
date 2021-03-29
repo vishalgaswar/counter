c1ad390689139240bd08a774cf261420adeacb8a
Vishal G. Aswar <vishalaswar25@gmail.com>
2021-03-29 14:02:36 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+//function for each btn
 btns.forEach(function (btn) {
    btn.addEventListener('click', function(e){
       const styles = e.currentTarget.classList;  
